3c40b7e27609afca67cd84dc90f80a45636d05c2
Brahian <147120513+BR4HI4N@users.noreply.github.com>
2023-11-22 08:25:59 -0300

    se agrego envio de formulario en javascript

--- script.js
+++ script.js
@@ -39,3 +39,27 @@ function seleccionar(){
 window.onscroll = function(){
 	efectoHabilidades();
 }
+
+
+// enviar formulario 
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+    event.preventDefault();
+
+	   btn.value = 'Enviando...';
+
+	      const serviceID = 'default_service';
+		     const templateID = 'template_fxifysy';
+
+			    emailjs.sendForm(serviceID, templateID, this)
+				    .then(() => {
+					      btn.value = 'Enviar Mensaje';
+						        alert('Mensaje enviado correctamente!');
+								    }, (err) => {
+									      btn.value = 'Enviar Mensaje';
+										        alert(JSON.stringify(err));
+												    });
+													});
